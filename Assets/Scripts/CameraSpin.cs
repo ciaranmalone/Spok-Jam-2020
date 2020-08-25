e650ee021756cf5048228f675fbb49fe143b8f23
ciaran.malone <ciaran.malone@Chemishield.com>
2020-08-25 23:03:37 +0100

    camera spin and more camera security

--- Assets/Scripts/CameraSpin.cs
+++ Assets/Scripts/CameraSpin.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class CameraSpin : MonoBehaviour
 {
+    [SerializeField] private float angleA = 90f;
+        [SerializeField] private float angleB = 0f;
+
     private void Start() {
         StartCoroutine(rotateCamera());
     }
 
     IEnumerator rotateCamera() {
         yield return new WaitForSeconds(5);
-        LeanTween.rotateY(gameObject, 90f, 2f);
+        LeanTween.rotateY(gameObject, angleA, 2f);
 
         yield return new WaitForSeconds(5);
-        LeanTween.rotateY(gameObject, 0f, 2f);
+        LeanTween.rotateY(gameObject, angleB, 2f);
 
         StartCoroutine(rotateCamera());
 
