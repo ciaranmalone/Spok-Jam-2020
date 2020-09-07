3ac841d8ad46ff862397680461605bee100fdb02
Evan McConnell <mcconnell.evan@gmail.com>
2020-09-07 22:41:28 +0100

    MOM PLEASE I DON'T WANT TO BE A PORTAL

---
+++ Assets/Scripts/Portals/PortalCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        transform.Rotate(new Vector3(0, 180, 0));
+        /*if(transform.rotation.eulerAngles.x > 0)
+        {
+            transform.Rotate(new Vector3(transform.rotation.eulerAngles.x * -1, 0, 0));
+        }*/
+    }
+}
