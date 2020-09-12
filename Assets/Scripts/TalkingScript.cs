f3b5dac4ec63e99b2dd7d9a6bd46ed8e101605ef
Evan McConnell <mcconnell.evan@gmail.com>
2020-09-12 23:40:08 +0100

    eat ur fruits kids, OR THERE'LL BE NO AMBIENT SOUNDS!!!

---
+++ Assets/Scripts/TalkingScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TalkingScript : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] AudioClips;
+    AudioSource AudioSource;
+    [SerializeField] private string[] subtitles;
+    [SerializeField] private TextMeshProUGUI mtext;
+
+    private bool playedAudio = false;
+
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+        mtext.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other) 
+   {
+       if(other.tag == "Player" && !playedAudio){
+            StartCoroutine(dialBegin());
+            StartCoroutine(enterStore());
+            playedAudio = true;
+       }
+   }
+
+    IEnumerator dialBegin() {
+        for(int i = 0; i < AudioClips.Length; i++)
+        {
+            AudioSource.clip = AudioClips[i];
+            mtext.text = subtitles[i];
+            AudioSource.Play();
+            yield return new WaitForSeconds(AudioSource.clip.length);   
+        }
+        mtext.text = "";
+        StartCoroutine(leaveStore());    
+    }
+
+    IEnumerator enterStore() {
+        yield return new WaitForSeconds(1f); 
+        LeanTween.moveLocal(gameObject, new Vector3(-55f, 2f, -31f), 5f).setEaseInSine();
+        yield return new WaitForSeconds(5f);   
+        LeanTween.moveLocal(gameObject, new Vector3(-55f, 2f, -15f), 5f).setEaseOutCirc();
+
+    }
+    IEnumerator leaveStore() {
+        LeanTween.moveLocal(gameObject, new Vector3(-55f, 2f, -139f), 10f).setEaseInCirc();
+        yield return new WaitForSeconds(10f);   
+        Destroy(gameObject);    
+    }
+}
---
+++ Assets/Scripts/disappearContoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappearContoller : MonoBehaviour
+{
+    [SerializeField] private string phase;
+    [SerializeField] private GameObject hiddenObjects;
+    void Start()
+    {
+        GameEvents.current.onPhaseChange += handleDisappear;
+    }
+    void handleDisappear(string phase)
+    {
+        if(phase == this.phase) {
+            hiddenObjects.SetActive(false);
+            GameEvents.current.onPhaseChange -= handleDisappear;
+        }
+    }
+}
