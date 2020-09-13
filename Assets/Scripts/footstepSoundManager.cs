d31bdada198cdb15f69b14b276fc56e0abc19275
Evan McConnell <mcconnell.evan@gmail.com>
2020-09-13 22:51:26 +0100

    my feet make sounds wow very cool thank you kanye

--- Assets/Scripts/footstepSoundManager.cs
+++ Assets/Scripts/footstepSoundManager.cs
@@ -10,17 +10,63 @@ public class footstepSoundManager : MonoBehaviour
     AudioSource[] sources;
     [SerializeField]
     float volume;
-    public int floorType;
+    [SerializeField]
+    float floorCheckLength;
+    public int soundType;
+    [SerializeField]
+    string floorType;
+    RaycastHit floorCheckHit;
+    [SerializeField]
+    LayerMask floorCheckMask;
     // Update is called once per frame
     void Update()
     {
+        Physics.Raycast(transform.position, -transform.up, out floorCheckHit, floorCheckLength, floorCheckMask);
+        floorType = floorCheckHit.collider.gameObject.tag;
+        switch(floorType)
+        {
+            case ("grass"):
+                soundType = 0;
+                break;
+            case ("tile"):
+                soundType = 1;
+                break;
+            case ("path"):
+                soundType = 2;
+                break;
+        }
         if(movementScript.isWalking == true)
         {
-            sources[floorType].volume = volume;
+            switch (soundType)
+            {
+                case (0):
+                    sources[0].volume = volume;
+                    sources[1].volume = 0;
+                    sources[2].volume = 0;
+                    break;
+                case (1):
+                    sources[0].volume = 0;
+                    sources[1].volume = volume;
+                    sources[2].volume = 0;
+                    break;
+                case (2):
+                    sources[0].volume = 0;
+                    sources[1].volume = 0;
+                    sources[2].volume = volume;
+                    break;
+            }
+            sources[soundType].volume = volume;
         }
         else
         {
-            sources[floorType].volume = 0;
+            sources[0].volume = 0;
+            sources[1].volume = 0;
+            sources[2].volume = 0;
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, -transform.up * floorCheckLength);
     }
 }
