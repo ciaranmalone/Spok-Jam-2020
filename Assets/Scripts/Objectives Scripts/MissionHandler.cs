f13a6d1f9e258e8c8894487e826372d0bc745311
Kigaru <jakubster9@gmail.com>
2020-09-15 00:08:33 +0100

    enhanced missions + storage of shop

---
+++ Assets/Scripts/Objectives Scripts/MissionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] phases;
+
+    public void startPhase(int phase)
+    {
+        phases[phase].GetComponent<MissionScript>().MakeObjectives();
+    }
+
+    public void clearPhase(int phase)
+    {
+        if (phase < 0) return;
+        phases[phase].GetComponent<MissionScript>().ClearObjectives();
+    }
+}
