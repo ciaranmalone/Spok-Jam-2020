c7be71887c21cc3e040b89fb211940763a9b7046
ciaran.malone <ciaran.malone@Chemishield.com>
2020-09-16 21:50:17 +0100

    The second dimension

---
+++ Assets/AppearOtherDimension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearOtherDimension : MonoBehaviour
+{
+    [SerializeField] GameObject OtherDimension;
+    
+    private void Start() {
+        OtherDimension.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other) {
+        OtherDimension.SetActive(true);
+
+    }
+}
---
+++ Assets/hideOnLeave.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideOnLeave : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        print("disappear");
+        this.transform.parent.gameObject.SetActive(false);
+    }
+}
