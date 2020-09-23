4043e8dd1f46605a77bec5f36c89c667e8931842
Evan McConnell <mcconnell.evan@gmail.com>
2020-09-24 00:07:44 +0100

    bitta sounds and maybe something else I'm really tired and oh yeah I changed the asphalt texture

--- Assets/_Scripts/autoDoorsOpen.cs
+++ Assets/_Scripts/autoDoorsOpen.cs
@@ -6,10 +6,19 @@ public class autoDoorsOpen : MonoBehaviour
 {
     [SerializeField] private GameObject doorLeft;
     [SerializeField] private GameObject doorRight;
+    [SerializeField] AudioClip[] sounds;
+    private AudioSource src;
+
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
    private void OnTriggerEnter(Collider other) 
    {
      LeanTween.moveLocal(doorLeft, new Vector3(-3, 0, 0), .5f).setEaseOutCubic();
      LeanTween.moveLocal(doorRight, new Vector3(6, 0, 0), .5f).setEaseOutCubic();
+        src.PlayOneShot(sounds[0]);
    }
 
    private void OnTriggerStay(Collider other) {
@@ -21,6 +30,7 @@ public class autoDoorsOpen : MonoBehaviour
    {
      LeanTween.moveLocal(doorLeft, new Vector3(0, 0, 0), .5f).setEaseOutCubic();
      LeanTween.moveLocal(doorRight, new Vector3(3, 0, 0), .5f).setEaseOutCubic();
+        src.PlayOneShot(sounds[1]);
 
-   }
+    }
 }
